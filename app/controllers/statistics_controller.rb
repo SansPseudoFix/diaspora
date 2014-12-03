6e06c927965e1c3db97d17bf17c9da00bd9cfda8
SansPseudoFix <spf@sanspseudofix.fr>
2014-12-03 18:07:18 +0100

    modified:   app/controllers/statistics_controller.rb 	modified:   app/views/publics/statistics.haml

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,15 +1,61 @@
-#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
+	
 
-class StatisticsController < ApplicationController
-
-  respond_to :html
-  
-  def statistics
-    respond_to do |format|
-      format.all { @css_framework = :bootstrap; render :template=>'publics/statistics', :layout => "application"}
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+    #   licensed under the Affero General Public License version 3 or later.  See
+    #   the COPYRIGHT file.
+     
+    class StatisticsController < ApplicationController
+     
+      @@result = {
+        'name' => AppConfig.settings.pod_name,
+        'network' => "Diaspora",
+        'version' => AppConfig.version_string,
+        'registrations_open' => AppConfig.settings.enable_registrations,
+        'services' => []
+      }
+     
+      respond_to :html
+     
+      def statistics
+        _statistics
+        @as_html = self.as_html
+        respond_to do |format|
+          format.all { @css_framework = :bootstrap; render :template=>'publics/statistics', :layout => "application"}
+        end
+      end
+     
+      def as_html
+        "Name: #{@@result['name']}
+       Network: #{@@result['network']}
+       "
+      end
+     
+     
+     
+      def _statistics
+        if AppConfig.privacy.statistics.user_counts?
+          @@result['total_users'] = User.count
+          @@result['active_users_halfyear'] = User.halfyear_actives.count
+          @@result['active_users_monthly'] = User.monthly_actives.count
+        end
+        if AppConfig.privacy.statistics.post_counts?
+          @@result['local_posts'] = self.local_posts
+        end
+        if AppConfig.privacy.statistics.comment_counts?
+          @@result['local_comments'] = self.local_comments
+        # end
+        #   @@result["services"] = Configuration::KNOWN_SERVICES.select {|service| AppConfig["services.#{service}.enable"]}.map(&:to_s)
+        #   Configuration::KNOWN_SERVICES.each do |service, options|
+        #     @@result[service.to_s] = AppConfig["services.#{service}.enable"]
+        end
+      end
+     
+      def local_posts
+        Post.where(:type => "StatusMessage").joins(:author).where("owner_id IS NOT null").count
+      end
+      def local_comments
+        Comment.joins(:author).where("owner_id IS NOT null").count
+      end
+     
     end
-  end
-  
-end
+
