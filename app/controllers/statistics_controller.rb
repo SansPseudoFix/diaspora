7b3f5a25d8ee8c2a1cb0a410c227d9abc87b09b6
Augier <AugierLe42e@users.noreply.github.com>
2014-12-08 15:09:49 +0100

    Update statistics_controller.rb

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -7,16 +7,17 @@ class StatisticsController < ApplicationController
   respond_to :html
  
   def statistics
-   @as_name = self.as_name(self.datas)
-   @as_network = self.as_network(self.datas)
-   @as_users = self.as_users(self.datas)
-   @as_active_users = self.as_active_users(self.datas)
-   @as_monthly_users = self.as_monthly_users(self.datas)
-   @as_posts = self.as_posts(self.datas)
-   @as_comments = self.as_comments(self.datas)
-   @as_version = self.as_version(self.datas)
-   @as_registrations = self.as_registrations(self.datas)
-   @as_services = self.as_services(self.datas)
+   self.datas
+   @as_name = @@result['name']
+   @as_network = @@result['network']
+   @as_users = @@result['total_users']
+   @as_active_users = @@esult['active_users_halfyear']
+   @as_monthly_users =  @@result['active_users_monthly']
+   @as_posts = @@result['local_posts']
+   @as_comments = @@result['local_comments']
+   @as_version = @@result['version']
+   @as_registrations = @@result['registrations_open']
+   @as_services = @@result['services']
    respond_to do |format|
     format.all { @css_framework = :bootstrap; render :template=>'publics/statistics', :layout => "application"}
    end
@@ -63,7 +64,7 @@ class StatisticsController < ApplicationController
   end
  
   def datas
-   result = {
+   @@result = {
     'name' => AppConfig.settings.pod_name,
     'network' => "Diaspora",
     'version' => AppConfig.version_string,
@@ -72,17 +73,17 @@ class StatisticsController < ApplicationController
    }
    
    if AppConfig.privacy.statistics.user_counts?
-     result['total_users'] = User.count
-     result['active_users_halfyear'] = User.halfyear_actives.count
-     result['active_users_monthly'] = User.monthly_actives.count
+     @@result['total_users'] = User.count
+     @@result['active_users_halfyear'] = User.halfyear_actives.count
+     @@result['active_users_monthly'] = User.monthly_actives.count
    end
    
    if AppConfig.privacy.statistics.post_counts?
-    result['local_posts'] = self.local_posts
+    @@result['local_posts'] = self.local_posts
    end
    
    if AppConfig.privacy.statistics.comment_counts?
-    result['local_comments'] = self.local_comments
+    @@result['local_comments'] = self.local_comments
    end
    #   @@result["services"] = Configuration::KNOWN_SERVICES.select {|service| AppConfig["services.#{service}.enable"]}.map(&:to_s)
    #   Configuration::KNOWN_SERVICES.each do |service, options|
