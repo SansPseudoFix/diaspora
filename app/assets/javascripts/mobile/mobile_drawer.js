80d810db4e492d6a3f3bb03e3f06625c26db0fbf
SansPseudoFix <spf@sanspseudofix.fr>
2017-01-31 15:15:40 +0100

    Refactor, more partials + No version check for mobile

--- app/assets/javascripts/mobile/mobile_drawer.js
+++ app/assets/javascripts/mobile/mobile_drawer.js
@@ -1,17 +1,22 @@
-(function(){
+(function() {
   Diaspora.Mobile.Drawer = {
-    initialize: function(){
-      $("#all_aspects").bind("tap click", function(evt){
+    initialize: function() {
+      $("#all_aspects").bind("tap click", function(evt) {
         evt.preventDefault();
         $(this).find("+ li").toggleClass("hide");
       });
 
-      $("#menu-badge").bind("tap click", function(evt){
+      $("#menu-badge").bind("tap click", function(evt) {
         evt.preventDefault();
         $("#app").toggleClass("draw");
       });
 
-      $("#followed_tags").bind("tap click", function(evt){
+      $("#followed_tags").bind("tap click", function(evt) {
+        evt.preventDefault();
+        $(this).find("+ li").toggleClass("hide");
+      });
+
+      $("#admin").bind("tap click", function(evt) {
         evt.preventDefault();
         $(this).find("+ li").toggleClass("hide");
       });
